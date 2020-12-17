0d4f124176c1954a200e265278186a7c11c18c6d
edmaszi <adam.attila.szikes@ericsson.com>
2020-12-17 14:29:22 +0100

    added go-mode without config
    Change-Id: I0c909ff1ca9e017a27aeea5ac1c1f00585a35ee4

--- config/custom.el
+++ config/custom.el
@@ -13,7 +13,7 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(c-basic-offset 2 t)
+ '(c-basic-offset 2)
  '(custom-enabled-themes '(manoj-dark))
  '(custom-safe-themes
    '("a1289424bbc0e9f9877aa2c9a03c7dfd2835ea51d8781a0bf9e2415101f70a7e" default))
@@ -22,7 +22,7 @@
  '(lsp-clients-clangd-args '("--background-index" "--clang-tidy" "-j=80"))
  '(lsp-ui-peek-enable t)
  '(package-selected-packages
-   '(lsp-java amx which-key lsp-ui auto-compile counsel counsel-projectile counsel-test ivy lsp-ivy swiper company-lsp lsp-mode projectile ztree anzu yasnippet company-flx helpful cmake-font-lock flycheck-pycheckers python-mode flycheck company transient magit lice json-mode dash)))
+   '(go-mode lsp-java amx which-key lsp-ui auto-compile counsel counsel-projectile counsel-test ivy lsp-ivy swiper company-lsp lsp-mode projectile ztree anzu yasnippet company-flx helpful cmake-font-lock flycheck-pycheckers python-mode flycheck company transient magit lice json-mode dash)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
