d76ecd736512be60674c1d6355de12aaea97009d
edmaszi <adam.attila.szikes@ericsson.com>
2020-12-18 14:31:24 +0100

    go-mode: tab-size 2 and tab indentation by default
    Change-Id: I411eeeaf32c22ac71f93ea385a196e87dae8b624

--- init.el
+++ init.el
@@ -297,6 +297,8 @@ Otherwise `c-or-c++-mode' decides."
 ;; set up before-save hooks to format buffer and add/delete imports.
 (add-hook 'go-mode-hook
   (lambda ()
+    (setq tab-width 2)
+    (setq indent-tabs-mode 1)
     (add-hook 'before-save-hook #'lsp-format-buffer t t)
     (add-hook 'before-save-hook #'lsp-organize-imports t t)))
 
