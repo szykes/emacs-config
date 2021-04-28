60b7fd7d2bfbaa20b1d1f92222b20a1fd69477e4
Ádám Szikes <szikes.adam@gmail.com>
2021-04-28 13:37:52 +0200

    Added yaml-mode

--- init.el
+++ init.el
@@ -324,5 +324,20 @@ Otherwise `c-or-c++-mode' decides."
 
 (add-hook 'yang-mode-hook #'flycheck-mode)
 
+
+;;; yaml
+
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
+
+;; Unlike python-mode, this mode follows the Emacs convention of not
+;; binding the ENTER key to `newline-and-indent'.  To get this
+;; behavior, add the key definition to `yaml-mode-hook':
+(add-hook 'yaml-mode-hook
+  '(lambda ()
+     (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
+
 (provide 'init.el)
 ;;; init.el ends here
