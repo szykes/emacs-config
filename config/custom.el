def92106dec95315e66c9d0b57ee7b7da2c1e1a5
szykes <szikes.adam@macbook>
2023-12-10 21:19:29 +0100

    Alig C settings

--- config/custom.el
+++ config/custom.el
@@ -13,18 +13,19 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(c-basic-offset 2 t)
  '(custom-enabled-themes '(manoj-dark))
  '(custom-safe-themes
    '("a1289424bbc0e9f9877aa2c9a03c7dfd2835ea51d8781a0bf9e2415101f70a7e" default))
  '(js-indent-level 2)
  '(json-reformat:indent-width 2)
  '(lsp-clients-clangd-args
-   '("--background-index" "--clang-tidy" "-j=14" "--header-insertion=never"))
+   '("--background-index" "--clang-tidy" "-j=3" "--header-insertion=never"))
  '(lsp-file-watch-threshold 10000)
  '(lsp-ui-peek-enable t)
  '(package-selected-packages
-   '(protobuf-mode rg flycheck-golangci-lint gn-mode dockerfile-mode lsp-pyright magit groovy-mode yaml-mode flycheck-yang yang-mode go-mode amx which-key lsp-ui auto-compile counsel counsel-projectile counsel-test ivy lsp-ivy swiper lsp-mode projectile ztree anzu yasnippet company-flx helpful cmake-font-lock flycheck-pycheckers python-mode flycheck company transient lice json-mode dash)))
+   '(protobuf-mode rg flycheck-golangci-lint gn-mode dockerfile-mode lsp-pyright magit groovy-mode yaml-mode flycheck-yang yang-mode go-mode amx which-key lsp-ui auto-compile counsel counsel-projectile counsel-test ivy lsp-ivy swiper lsp-mode projectile ztree anzu yasnippet company-flx helpful cmake-font-lock flycheck-pycheckers python-mode flycheck company transient lice json-mode dash))
+ '(warning-suppress-log-types '((comp)))
+ '(warning-suppress-types '((auto-save) (comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
