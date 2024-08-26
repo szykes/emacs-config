88f2552e670df3ca2da9648491c41af4281a26d8
aszikes <aszikes@cisco.com>
2024-08-26 13:01:48 +0200

    Increase copilot prompt size

--- init.el
+++ init.el
@@ -267,6 +267,7 @@ will be killed."
   (require 'copilot)
 
   (setq copilot-indent-offset-warning-disable t)
+  (setq copilot-max-characters 500000)
   (add-hook 'prog-mode-hook 'copilot-mode)
   (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
   (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
