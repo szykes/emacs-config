1e399f264829d0de1d74d5c0c7e8c7c3b975fd81
Szikes Ádám <adam@Riltech-MBP.local>
2025-07-25 08:52:39 +0200

    format and add some packages

--- config/custom.el
+++ config/custom.el
@@ -12,19 +12,30 @@
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
-   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3"
+    "DeepSkyBlue" "gray50"])
  '(custom-enabled-themes '(manoj-dark))
  '(custom-safe-themes
-   '("a1289424bbc0e9f9877aa2c9a03c7dfd2835ea51d8781a0bf9e2415101f70a7e" default))
+   '("a1289424bbc0e9f9877aa2c9a03c7dfd2835ea51d8781a0bf9e2415101f70a7e"
+     default))
  '(flycheck-golangci-lint-disable-all t)
  '(js-indent-level 2)
  '(json-reformat:indent-width 2)
+ '(lsp-buf-args '("beta" "lsp" "--timeout" "0" "--log-format" "text"))
  '(lsp-clients-clangd-args
-   '("--background-index" "--clang-tidy" "-j=3" "--header-insertion=never"))
+   '("--background-index" "--clang-tidy" "-j=3"
+     "--header-insertion=never"))
  '(lsp-file-watch-threshold 20000)
  '(lsp-ui-peek-enable t)
  '(package-selected-packages
-   '(kubedoc kubernetes f editorconfig protobuf-mode rg flycheck-golangci-lint gn-mode dockerfile-mode lsp-pyright magit groovy-mode yaml-mode flycheck-yang yang-mode go-mode amx which-key lsp-ui auto-compile counsel counsel-projectile counsel-test ivy lsp-ivy swiper lsp-mode projectile ztree anzu yasnippet company-flx helpful cmake-font-lock flycheck-pycheckers python-mode flycheck company transient lice json-mode dash))
+   '(amx anzu auto-compile cmake-font-lock company company-flx counsel
+         counsel-projectile counsel-test csv-mode dash dockerfile-mode
+         editorconfig f flycheck flycheck-golangci-lint
+         flycheck-pycheckers flycheck-yang gn-mode go-mode groovy-mode
+         helpful ivy json-mode kubedoc kubernetes lice lsp-ivy
+         lsp-mode lsp-pyright lsp-ui magit nlinum projectile
+         protobuf-mode python-mode rg swiper transient which-key
+         yaml-mode yang-mode yasnippet ztree))
  '(warning-suppress-log-types '((comp)))
  '(warning-suppress-types '((auto-save) (comp))))
 (custom-set-faces
